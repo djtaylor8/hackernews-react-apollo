fee028b73644523ddb988b7db540a44f675c0779
DJ <dtaylor88@gmail.com>
2021-11-11 09:53:45 -0800

    add login component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
 import { Routes, Route } from 'react-router-dom';
+import Login from './Login';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
             path="/create"
             element={<CreateLink/>}
           />
+          <Route exact path='/login' element={<Login />} />
         </Routes>
       </div>
     </div>
